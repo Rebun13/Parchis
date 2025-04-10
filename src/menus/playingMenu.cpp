d5d4d5b40816261396d7130034602b7ab842a9e1
Rebun13 <rebun@tutamail.com>
2025-04-10 18:04:37 +0200

    Added "Surrender" popup It doesn't work :P Ruben Gomez Dopazo

--- src/menus/playingMenu.cpp
+++ src/menus/playingMenu.cpp
@@ -3,6 +3,7 @@
 PlayingMenu::PlayingMenu()
 {
     font = LoadFont("fonts/JetSet-8j1J.ttf");
+    exitWindowTextSize = MeasureTextEx(font, exitWindowText, 14, 1);
 }
 
 PlayingMenu::~PlayingMenu()
@@ -16,29 +17,46 @@ void PlayingMenu::draw()
     {
         exitButtonClicked = true;
     }
+
     if (GuiButton(buttonSettings, settingsButtonText))
     {
         settingsButtonClicked = true;
+    }
+
+    if(exitButtonClicked) {
+        GuiPanel(exitWindow, "");
+        GuiLabel({(480 - exitWindowTextSize.x) / 2, exitWindowTextPosition.y}, exitWindowText);
+        if(GuiButton(buttonSurrender, surrenderButtonText)) {
+            surrenderButtonClicked = true;
+        };
+        if(GuiButton(buttonCancel, cancelButtonText)) {
+            cancelButtonClicked = true;
+        };
     }
 }
 
 unsigned char PlayingMenu::onTouch()
 {
+    if (surrenderButtonClicked)
+    {
+        reset();
+        return EXIT_BUTTON;
+    }
     if (settingsButtonClicked)
     {
         reset();
         return SETTINGS_BUTTON;
     }
-    if (settingsButtonClicked)
-    {
+    if(cancelButtonClicked) {
         reset();
-        return EXIT_BUTTON;
     }
     return -1;
 }
 
 void PlayingMenu::reset()
 {
+    surrenderButtonClicked = false;
     settingsButtonClicked = false;
-    settingsButtonClicked = false;
+    cancelButtonClicked = false;
+    exitButtonClicked = false;
 }
--- src/menus/playingMenu.h
+++ src/menus/playingMenu.h
@@ -18,12 +18,23 @@ public:
 private:
     const char* settingsButtonText = GuiIconText(ICON_SETTINGS, "");
     const char* exitButtonText = "EXIT";
+    const char* exitWindowText = "Are you sure you want to surrender?";
+    const char* surrenderButtonText = "SURRENDER";
+    const char* cancelButtonText = "CANCEL";
 
-    const Rectangle buttonSettings = {400, 20, 60, 60};
-    const Rectangle buttonExit = {20, 20, 60, 60};
+    const Rectangle buttonSettings = {400, 20, 60, 40};
+    const Rectangle buttonExit = {20, 20, 60, 40};
+    
+    const Rectangle exitWindow = {20, 200, 440, 160};
+    const Vector2 exitWindowTextPosition = {40, 180};
+    const Rectangle buttonSurrender = {20, 210, 180, 40};
+    const Rectangle buttonCancel = {240, 210, 180, 40};
 
     bool settingsButtonClicked = false;
     bool exitButtonClicked = false;
+    bool surrenderButtonClicked = false;
+    bool cancelButtonClicked = false;
 
     Font font;
+    Vector2 exitWindowTextSize;
 };
